4612221f39594cf6c79436d9160892b24dc9a6cc
Dreamice <dreamice13@foxmail.com>
2024-02-29 22:38:26 +0800

    feat: primitive模块(面、椭圆)

--- src/views/GeomPrimitive.vue
+++ src/views/GeomPrimitive.vue
@@ -2,7 +2,7 @@
  * @Author: Dreamice dreamice13@foxmail.com
  * @Date: 2024-02-29 08:52:45
  * @LastEditors: Dreamice dreamice13@foxmail.com
- * @LastEditTime: 2024-02-29 09:41:52
+ * @LastEditTime: 2024-02-29 17:30:41
  * @FilePath: \CesiumShow\src\views\GeomPrimitive.vue
  * @Description: Primitive相关功能
 -->
@@ -37,7 +37,44 @@ onMounted(() => {
         appearance: appePolyline,
     })
     viewer.scene.primitives.add(primitivePolyline);
-
+    // 面
+    const geomInsPolygon = new Cesium.GeometryInstance({
+        geometry: new Cesium.PolygonGeometry({
+            polygonHierarchy: new Cesium.PolygonHierarchy(
+                Cesium.Cartesian3.fromDegreesArray([
+                    117.028019,36.638372, 
+                    117.021877,36.637955,
+                    117.022346,36.630539,
+                    117.029848,36.631216
+                ])
+            )
+        })
+    })
+    const appePolygon = new Cesium.MaterialAppearance({
+        material: Cesium.Material.fromType('Dot')
+    })
+    const primitivePolygon = new Cesium.Primitive({
+        geometryInstances: geomInsPolygon,
+        appearance: appePolygon
+    })
+    viewer.scene.primitives.add(primitivePolygon)
+    // 椭圆
+    const geomInsEllipse = new Cesium.GeometryInstance({
+        geometry: new Cesium.EllipseGeometry({
+            center: Cesium.Cartesian3.fromDegrees(117.047632, 36.617403),
+            semiMajorAxis: 500,
+            semiMinorAxis: 300
+        })
+    })
+    const appeEllipse = new Cesium.EllipsoidSurfaceAppearance({
+        material: Cesium.Material.fromType('Stripe')
+    })
+    const primitiveEllipse = new Cesium.Primitive({
+        geometryInstances: geomInsEllipse,
+        appearance: appeEllipse
+    })
+    viewer.scene.primitives.add(primitiveEllipse);
+    
     viewer.camera.setView({
         // Cesium的坐标是以地心为原点，一向指向南美洲，一向指向亚洲，一向指向北极州
         // fromDegrees()方法，将经纬度和高程转换为世界坐标
