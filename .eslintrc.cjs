596afc1973116218d495b8aa0895ffb76da1028c
Dreamice <dreamice13@foxmail.com>
2024-03-01 22:34:04 +0800

    添加ESLint校验插件

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/*
+ * @Author: Dreamice dreamice13@foxmail.com
+ * @Date: 2024-03-04 17:48:49
+ * @LastEditors: Dreamice dreamice13@foxmail.com
+ * @LastEditTime: 2024-03-04 18:29:16
+ * @FilePath: \CesiumShow\.eslintrc.cjs
+ * @Description: 
+ */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential'
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
