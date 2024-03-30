4c7ae44a022e30238be0c328e9bc7da1534563e1
Dreamice <dreamice13@foxmail.com>
2024-03-30 22:30:34 +0800

    feat: add fog

---
+++ src/views/FogShow.vue
@@ -0,0 +1,22 @@
+<script setup>
+import * as Cesium from 'cesium'
+import { onMounted } from 'vue'
+
+Cesium.Ion.defaultAccessToken=import.meta.env.VITE_TOKEN_CESIUM
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer')
+  viewer.scene.debugShowFramesPerSecond = true
+})
+
+</script>
+
+<template>
+    <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+    width: 100%;
+    height: 100%;
+}
+</style>
