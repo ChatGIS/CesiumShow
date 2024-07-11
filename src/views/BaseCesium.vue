5002da5e5264065bfffe66f719cb726f68ebf964
ChatGIS <ChatGIS@outlook.com>
2024-07-11 22:24:45 +0800

    add camera

--- src/views/BaseCesium.vue
+++ src/views/BaseCesium.vue
@@ -1,19 +1,27 @@
+<template>
+    <div id="cesiumContainer"></div>
+</template>
 <script setup>
 import * as Cesium from 'cesium'
 import { onMounted } from 'vue'
 
 Cesium.Ion.defaultAccessToken=import.meta.env.VITE_TOKEN_CESIUM
-onMounted(() => {
-  const viewer = new Cesium.Viewer('cesiumContainer')
+let viewer
+onMounted(async () => {
+  viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: await Cesium.createWorldTerrainAsync()
+  })
   viewer.scene.debugShowFramesPerSecond = true
+  viewer.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(117.027247, 36.650047, 500),
+    orientation: {
+      heading: Cesium.Math.toRadians(161.69687),
+      pitch: Cesium.Math.toRadians(-14.366),
+      roll: Cesium.Math.toRadians(0)
+    }
+  })
 })
-
 </script>
-
-<template>
-    <div id="cesiumContainer"></div>
-</template>
-
 <style scoped>
 #cesiumContainer {
     width: 100%;
